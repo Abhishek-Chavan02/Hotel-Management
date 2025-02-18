c8e69a95ac1f97a76d49fbf37c7362a5666c3a13
Abhishek Devidas Chavan <143535894+Abhishek-Chavan02@users.noreply.github.com>
2025-02-18 20:25:51 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import BookingTabs from "./components/BookingTabs";
+import Welcome from "./Welcome";
+
+function App() {
+  return (
+    <div>
+      {/* <Welcome /> */}
+      <BookingTabs />
+    </div>
+  );
+}
+
+export default App;
