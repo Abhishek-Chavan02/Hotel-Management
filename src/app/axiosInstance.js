f1e786985e526d6cb40f358bb309dff9fc93bd09
Abhishek Devidas Chavan <143535894+Abhishek-Chavan02@users.noreply.github.com>
2025-02-24 20:37:36 +0530

    Add files via upload

---
+++ src/app/axiosInstance.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+// export const getUsers = () => {
+//     return axios.get("https://jsonplaceholder.typicode.com/users");
+
+// }
+
+ const axiosInstance = axios.create({
+    baseURL: 'https://some-domain.com/api/',
+    timeout: 1000,
+    // headers: {'X-Custom-Header': 'foobar'}
+  });
+
+  export default  axiosInstance
