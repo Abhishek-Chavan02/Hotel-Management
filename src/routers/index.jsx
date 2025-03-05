eed663a1964225592d6c36817180b8f06fa8db10
Abhishek Devidas Chavan <143535894+Abhishek-Chavan02@users.noreply.github.com>
2025-03-05 20:54:15 +0530

    Update index.jsx

--- src/routers/index.jsx
+++ src/routers/index.jsx
@@ -1,45 +1,82 @@
-import { Routes, Route } from 'react-router-dom';
-import Login from '../pages/Login';
-import SignUp from '../pages/SignUp';
-import Home from '../pages/Home';
-import Contact from '../pages/Contact';
-import Rooms from '../pages/Rooms';
-import AboutUs from '../pages/AboutUs';
-import Profile from '../pages/Profile';
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Login from '../pages/Login'
+import Home from '../pages/Home'
+import SignUp from '../pages/SignUp'
+import ProtectedRoutes from './ProtectedRoutes'
+import Dashboard from '../dashboard/Dashboard'
+import HotelList from '../dashboard/HotelList'
+import UserList from '../dashboard/UserList'
+import AdminLayout from '../layout/AdminLayout'
+import AppLayout from '../applayout/AppLayout'
+import Adminprofile from '../dashboard/Adminprofile'
+import Contact from '../pages/Contact'
+import AboutUs from '../pages/AboutUs'
+import Rooms from '../pages/Rooms'
+import Profile from '../pages/Profile'
+import News from '../pages/News'
 
-import ProtectedRoutes from './ProtectedRoutes';
-import AppLayout from '../applayout/AppLayout';
-import Adminprofile from '../dashboard/Adminprofile';
-import UserList from '../dashboard/UserList';
-import HotelList from '../dashboard/HotelList';
-import Dashboard from '../dashboard/Dashboard';
 
 function Router() {
     return (
         <Routes>
+
             <Route path="/" element={<Login />} />
             <Route path="/signup" element={<SignUp />} />
 
-            <Route element={<ProtectedRoutes />}>
-                <Route element={<AppLayout />}>
-                    <Route path="/home" element={<Home />} />
-                    <Route path="/contact" element={<Contact />} />
-                    <Route path="/rooms" element={<Rooms />} />
-                    <Route path="/about-us" element={<AboutUs />} />
-                    <Route path="/profile" element={<Profile />} />
-                </Route>
+
+            <Route path="/" element={<AppLayout />}>
+
+                <Route path="home" element={
+                    <ProtectedRoutes>
+                        <Home />
+                    </ProtectedRoutes>
+                } />
+                <Route path="contact" element={
+                    <ProtectedRoutes>
+                        <Contact />
+                    </ProtectedRoutes>
+                } />
+                <Route path="rooms" element={
+                    <ProtectedRoutes>
+                        <Rooms />
+                    </ProtectedRoutes>
+                } />
+                <Route path="about-us" element={
+                    <ProtectedRoutes>
+                        <AboutUs />
+                    </ProtectedRoutes>
+                } />
+                <Route path="profile" element={
+                    <ProtectedRoutes>
+                        <Profile />
+                    </ProtectedRoutes>
+                } />
+                <Route path="news" element={
+                    <ProtectedRoutes>
+                        <News />
+                    </ProtectedRoutes>
+                } />
+
             </Route>
 
-            {/* <Route element={<ProtectedRoutes />}>
-                <Route element={<AppLayout />}>
-                    <Route path="/admin" element={<Dashboard />} />
-                    <Route path="/admin/hotel-list" element={<HotelList />} />
-                    <Route path="/admin/user-list" element={<UserList />} />
-                    <Route path="/admin/admin-profile" element={<Adminprofile />} />
-                </Route>
-            </Route> */}
+
+            <Route path="/admin" element={<AdminLayout />}>
+                <Route path="dashboard" element={
+                    <Dashboard />
+                } />
+                <Route path="hotel-list" element={
+                    <HotelList />
+                } />
+                <Route path="user-list" element={
+                    <UserList />
+                } />
+                <Route path="admin-profile" element={
+                    <Adminprofile />
+                } />
+            </Route>
         </Routes>
-    );
+    )
 }
 
-export default Router;
+export default Router
